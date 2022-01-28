b3f8448dd91203ca4aa5ac4eb9b95cc2da39f108
dannymagnus <mrdanrichards@gmail.com>
2022-01-28 20:32:30 +0000

    add functionality to display data

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,14 +1,26 @@
+#__init__.py runs whenever the directory is imported by another file
+#Whatever is in _init__.py gets run so then the folder can be referenced
+#normal use would be from taskmanager.models import xxxx
+#with __init__.py becomes import taskmanager - then what is in taskmanager can be used
+# example here https://www.youtube.com/watch?v=cONc0NcKE7s
+#import os so that env variables can be accessed, this wont work once in git.
 import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+#only import the environmental variable if path exists (local environment)
 if os.path.exists("env.py"):
     import env  # noqa
 
 
+#default flask instructions for Flask instatiation
 app = Flask(__name__)
+#gives flask a secret key to work
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+#gives the url for the database
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes  # noqa
+#imports the routes py
+from taskmanager import routes #noqa
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,8 +1,31 @@
-from flask import render_template
+#importing the function to display html files as part of the flask route
+from flask import render_template, request, redirect, url_for
 
 from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
 def home():
-    return render_template('base.html')
+    return render_template('tasks.html')
+
+
+@app.route("/categories")
+def categories():
+    #Flask-SQLAlchemy query to query the Category model into cursor object (iterable variable)
+    #wrapped in list so that front end can deal with it, as cursor objects are tricky
+    categories = list(Category.query.order_by(Category.category_name).all())
+    #when a user clicks on categories link template is rendered
+    #1st categories is the categories that can be used in html template
+    #2nd is the categories variable in this function
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
